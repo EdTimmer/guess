f5c730ede0f258b0f77acdc65766d3780cfd3d19
Edward Timmer <edtimmer@gmail.com>
2019-03-10 08:30:44 -0400

    refactored box press function

--- src/App.js
+++ src/App.js
@@ -56,12 +56,12 @@ function App() {
   }
 
 
-  function pressOne() {
+  function press(num) {
 
     if (totalCount < 24) {
       let correctNumber = Math.floor(Math.random() * 4);
       setTotalCount(totalCount + 1);
-      if (correctNumber === 0) {
+      if (correctNumber === num) {
         setCorrectCount(correctCount + 1);
         messageFn();
         (async () => {
@@ -70,54 +70,6 @@ function App() {
             .then(() => setCorectGuess(false));
         })();
 
-      }
-    }
-  }
-
-  function pressTwo() {
-    if (totalCount < 24) {
-      let correctNumber = Math.floor(Math.random() * 4);
-      setTotalCount(totalCount + 1);
-      if (correctNumber === 1) {
-        setCorrectCount(correctCount + 1);
-        messageFn();
-        (async () => {
-          setCorectGuess(true);
-          await showPicture(1000)
-            .then(() => setCorectGuess(false));
-        })();
-      }
-    }
-  }
-
-  function pressThree() {
-    if (totalCount < 24) {
-      let correctNumber = Math.floor(Math.random() * 4);
-      setTotalCount(totalCount + 1);
-      if (correctNumber === 2) {
-        setCorrectCount(correctCount + 1);
-        messageFn();
-        (async () => {
-          setCorectGuess(true);
-          await showPicture(1000)
-            .then(() => setCorectGuess(false));
-        })();
-      }
-    }
-  }
-
-  function pressFour() {
-    if (totalCount < 24) {
-      let correctNumber = Math.floor(Math.random() * 4);
-      setTotalCount(totalCount + 1);
-      if (correctNumber === 3) {
-        setCorrectCount(correctCount + 1);
-        messageFn();
-        (async () => {
-          setCorectGuess(true);
-          await showPicture(1000)
-            .then(() => setCorectGuess(false));
-        })();
       }
     }
   }
@@ -142,7 +94,7 @@ function App() {
       </div>
       <div className="body">
         {
-          correctGuess ? <img src={pyramids} style={{ height: '600px', width: '600px' }} alt="reward" /> : <Boxes pressOne={pressOne} pressTwo={pressTwo} pressThree={pressThree} pressFour={pressFour} />
+          correctGuess ? <img src={pyramids} style={{ height: '600px', width: '600px' }} alt="reward" /> : <Boxes press={press} />
         }
         <div className="result">{totalCount} trials</div>
         <button className="reset" onClick={clearState}>Reset</button>
--- src/Boxes.js
+++ src/Boxes.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './App.css';
 // import Boxes from './Boxes';
 
-function Boxes({pressOne, pressTwo, pressThree, pressFour}) {
+function Boxes({press}) {
   
   return (      
        
     <div className="grid-container body">
 
-      <div className="grid-item one" onClick={pressOne} />
-      <div className="grid-item two" onClick={pressTwo} />
-      <div className="grid-item three" onClick={pressThree} />      
-      <div className="grid-item four" onClick={pressFour} />          
+      <div className="grid-item one" onClick={() => press(0)} />
+      <div className="grid-item two" onClick={() => press(1)} />
+      <div className="grid-item three" onClick={() => press(2)} />      
+      <div className="grid-item four" onClick={() => press(3)} />          
     
     </div>
     
